397beb0e63c940403e88c9852b234cf978a0cef0
Immanuel Van Meirhaeghe <immanuelvm@live.be>
2021-08-28 02:02:42 +0200

    latest

--- Enums.cs
+++ Enums.cs
@@ -2,385 +2,385 @@
 {
     public enum MapLocation
     {
-        Teleport_Start_Location = 0,
-        Bamboo_Bridge = 1,
-        East_Native_Camp = 2,
-        Elevator_Cave = 3,
-        Planecrash_Cave = 4,
-        Native_Passage = 5,
-        Overturned_Jeep = 6,
-        Abandoned_Tribal_Village = 7,
-        West_Native_Camp = 8,
-        Puddle = 9,
-        Harbor = 10,
-        Drug_Facility = 11,
-        Bamboo_Camp = 12,
-        Scorpion_Cartel_Cave = 13,
-        Airport = 14,
-        Tutorial_Camp = 15,
-        Omega_Camp = 16,
-        Main_Tribal_Village = 17,
-        Anaconda_Island = 18,
-        Pond = 19,
-        Story_Start_Oasis = 20,
-        Custom = 21,
-        PVE2_food_ration_hanging_02 = 22,
-        BadWater_05 = 23,
-        PVE2_food_ration_hanging_08 = 24,
-        food_ration_17_liane = 25,
-        Patrol_12_Totem_01 = 26,
-        A04S01_b = 27,
-        PVE2_food_ration_hanging_09 = 28,
-        LegendaryQuest_BabyTapirSpawner3 = 29,
-        PT_A03S03_TribeVillage = 30,
-        A01S12_WhaCaveCamp = 31,
-        food_ration_01_mound = 32,
-        LegendaryQuest_BadWaterCaveSpawner = 33,
-        A04S01_c_Barrel1 = 34,
-        food_ration_22_liane = 35,
-        PT_A01S05_River = 36,
-        PT_A01S06_Jungle = 37,
-        A04_S02_c_hot_river,
-        A01S07_Village,
-        A04_S01_f_hub_village,
-        Bike_01,
-        food_ration_04_mound,
-        PT_A01S08_PlaneCrash,
-        PoisonedWater10,
-        PVE2_Wounded_05_cave,
-        WomanInCage_02,
-        PoisonedWater06,
-        A04_S01_g_POI_on_hills,
-        PT_A02S02_Lake,
-        Bike_07,
-        A04_S01_g_passage_to_A01_S06,
-        BadWater_01,
-        PVE2_Wounded_04,
-        PT_A03S01_WHACamp,
-        A01S06_Cartel_Cave,
-        PVE2_SmallAICamp_18,
-        food_ration_24_liane,
-        PVE2_food_ration_axe,
-        PT_A01S01_TribeCamp,
-        PVE2_SmallAICamp_14,
-        food_ration_11_liane,
-        Cartel_Antena,
-        LegendaryQuest_PantherSpawnerOutside,
-        Canoe_01,
-        PoisonedWater05,
-        LegendaryQuest_BabyTapirSpawner6,
-        Drum_05,
-        A04_S01_c_waterfall_caves,
-        PT_A01S01_CaveCamp,
-        PVE2_Kid_02,
-        A04S01_g,
-        Patrol_11_Totem_01,
-        SmallAICamp_05,
-        PT_A01S08_Jungle,
-        HangedBody_08,
-        Bike_02,
-        PT_A01S10_TribeCamp,
-        PT_A01S09_Elevator,
-        SmallAICamp_01,
-        A04_S01_e_muddy_gorges,
-        A04_S01_e_giant_cave,
-        A04_S01_c_mangrove_border,
-        Caves_7,
-        LegendaryQuest_BabyTapirSpawner2,
-        food_ration_05_mound,
-        A01S07_RockToCartel,
-        LegendaryQuest_BabyTapirSpawner7,
-        PVE2_Wounded_04_cave,
-        Patrol_09_Totem_02,
-        PT_A02S01_Airport,
-        HangedBody_04,
-        Caves_1,
-        LegendaryQuest_BabyTapirSpawner4,
-        A04S01_d,
-        HangedBody_11,
-        PVE2_food_ration_08,
-        Panther_04,
-        PT_A01S06_Cartel,
-        Caves_12,
-        PVE2_Wounded_03,
-        A02S01_Cenot,
-        A01S07_GrabbingHook,
-        A04S02_c,
-        Patrol_06_Totem_02,
-        PT_A03S02_Tutorial,
-        Patrol_10_Totem_01,
-        Drum_08,
-        Bike_06,
-        Canoe_03,
-        Panther_01,
-        Albino_01,
-        PVE2_food_ration_hanging_10,
-        A04_S01_b_big_enemy_camp,
-        Panther_03,
-        PVE2_Wounded_03_cave,
-        PVE2_food_ration_hanging_07,
-        Statue_07,
-        PT_A03S01_Rozlewiska,
-        Caves_10,
-        PVE2_BigAICamp_05,
-        PVE2_BigAICamp_04,
-        PVE2_food_ration_hanging_05,
-        PT_A02S01_Cenote,
-        A03S01_StoneRing,
-        SmallAICamp_08,
-        WomanInCage_03,
-        A04_S01_f_shaman_passage,
-        Patrol_02_Totem_01,
-        Panther_07,
-        PT_A01S07_Jungle,
-        LegendaryQuest_BabyTapirSpawner8,
-        PoisonedWater07,
-        Caves_4,
-        Patrol_13_Totem_02,
-        Drum_01,
-        LegendaryQuest_BabyTapirSpawner5,
-        food_ration_13_mound,
-        HangedBody_07,
-        A03S01_Camp,
-        Patrol_05_Totem_01,
-        food_ration_09_liane,
-        Albino_03,
-        LegendaryQuest_AlbinoSpawner,
-        PoisonedWater09,
-        Wounded_04,
-        Statue_02,
-        food_ration_19_liane,
-        A01S10_BambooBridge,
-        ChallangeSP_MightyCamp,
-        Patrol_04_Totem_02,
-        PoisonedWater01,
-        Albino_04,
-        Albino_06,
-        LegendaryQuest_BabyTapirSpawner1,
-        PVE2_SmallAICamp_16,
-        PVE_Boat,
-        PVE2_food_ration_06,
-        A04S01_e_Barrel3,
-        PoisonedWater03,
-        PT_A01S03_Jungle,
-        PoisonedWater04,
-        A01S07_StoneRings,
-        PVE2_food_ration_07,
-        A04S02_b,
-        Patrol_10_Totem_03,
-        SmallAICamp_03,
-        PVE2_SmallAICamp_13,
-        PVE2_food_ration_hanging_06,
-        QA_Test,
-        PT_A01S03_Jeep,
-        PoisonedWater08,
-        food_ration_23_mound,
-        Patrol_09_Totem_01,
-        SmallAICamp_02,
-        A04S01_c,
-        A04S01_c_Destroy,
-        Statue_04,
-        BadWater_07,
-        A01S09_Elevator,
-        A03S01_CaveAyuhaska,
-        A04_S01_c_waterfalls_island,
-        LegendaryQuest_PantherSpawner,
-        A04_S01_f_passage_to_A01_S04,
-        SmallAICamp_09,
-        Kid_02,
-        food_ration_10_mound,
-        Patrol_14_Totem_02,
-        SmallAICamp_06,
-        PVE2_food_ration_11,
-        A04_S01_c_passage_to_A03_S03,
-        Caves_6,
-        PVE2_food_ration_13,
-        Panther_06,
-        food_ration_03_mound,
-        PVE2_food_ration_01,
-        A01S11_SQPlace,
-        A04_S01_d_pve_boat,
-        Statue_01,
-        PVE2_Kid_04,
-        Caves_15,
-        SmallAICamp_01_outer,
-        PT_A01S11_JungleBamboo,
-        food_ration_25_mound,
-        HangedBody_03,
-        HangedBody_13,
-        A04_S01_b_passage_to_A01_S07,
-        Hub_Village_Outside,
-        Debug,
-        Statue_05,
-        Panther_05,
-        PT_A01S07_Jungle2,
-        A04_S02_e_river_canyons,
-        ChallangeSP_Combat_WT,
-        food_ration_06_liane,
-        HangedBody_06,
-        PVE2_Kid_01,
-        A02S02_Pond,
-        PVE2_food_ration_09,
-        Patrol_08_Totem_02,
-        Canoe_05,
-        PVE2_Wounded_02_cave,
-        PVE2_BigAICamp_03,
-        PT_A01S01_Harbor,
-        Albino_02,
-        HangedBody_09,
-        Wounded_02,
-        PVE2_food_ration_05,
-        PVE2_SmallAICamp_20,
-        Caves_8,
-        A01S09_GoldMine,
-        A01S02,
-        SmallAICamp_10,
-        WomanInCage_01,
-        HangedBody_10,
-        A04S01_a,
-        food_ration_07_mound,
-        PVE2_SmallAICamp_11,
-        PVE2_Wounded_01,
-        A01S12_GHtoAirport,
-        Drum_03,
         A01S01_CaveCamp,
-        HangedBody_02,
-        BadWater_06,
-        MantisSpawner,
-        PT_A01S01_Jungle,
-        A01S03_Jeep,
-        Village_02_outside,
-        Canoe_04,
-        PT_A02S02_Cenote,
-        PVE2_SmallAICamp_17,
-        Caves_11,
-        A04_S01_b_big_caves,
-        Kid_04,
-        Village_02_inside,
-        food_ration_08_mound,
-        Bike_05,
-        Patrol_04_Totem_01,
-        food_ration_02_mound,
-        A04_S01_d_steamboat,
-        Caves_17,
-        Canoe_06,
-        A04_S02_b_stone_bridge,
-        PVE2_food_ration_10,
-        LegendaryQuest_CanoeBoatSpawner,
-        PT_A01S09_GoldMine,
-        BigAICamp_03,
-        Caves_14,
-        PVE2_SmallAICamp_12,
-        Wounded_01,
-        PVE2_food_ration_14,
-        food_ration_18_mound,
-        ChallangeSP_Combat_Battery,
-        Patrol_03_Totem_01,
-        Caves_5,
-        Caves_3,
-        Patrol_07_Totem_01,
         A01S01_EvilTribeCamp,
-        BigAICamp_02,
-        PT_A01S07_TribeVillage,
-        Caves_9,
-        Panther_02,
-        A01S06_Cartel,
-        A01S04,
-        Kid_03,
-        PVE2_food_ration_03,
-        BadWater_03,
-        food_ration_14_mound,
-        A01S11,
-        Kid_01,
-        PVE2_food_ration_12,
-        A04_S02_d_river_canyons_cave,
-        A01S12_Island,
-        A04_S02_e_big_swamps,
-        A04S02_e,
-        A01S05_Puddle,
-        food_ration_15_liane,
-        A01S11_EvilTribeCamp,
-        A02S01_Airport,
-        HangedBody_01,
-        food_ration_20_liane,
-        Patrol_08_Totem_01,
-        Spikes_Tree_Review_Spawner,
-        Statue_03,
-        PT_A01S05_Pond,
-        HangedBody_05,
-        PT_A01S12_WHACamp,
-        PVE_Map_Crate,
-        HangedBody_12,
-        A03S01_GrabbHookHigh,
-        PT_A03S03_Jungle,
-        A01S10_EvilTribeCamp,
-        food_ration_21_mound,
-        Patrol_05_Totem_02,
-        PT_A01S02_Jungle,
-        PT_A01S11_Jungle,
-        Statue_06,
-        Canoe_02,
-        Drum_07,
-        Bike_03,
-        PVE2_Wounded_02,
-        A04_S02_b_sacred_ruins,
-        A01S01_Harbor,
-        PT_A02S01_Jungle,
-        ChallangeSP_Raft,
-        A03S03_TribeVillage,
-        PVE_StartDebugSpawner,
-        Caves_13,
-        A04_S01_b_passage_to_A01_S12,
-        Patrol_12_Totem_02,
-        LegendaryQuest_BadWaterAltarSpawner,
-        A04_S02_c_dead_bodies_water_cave,
-        LegendaryQuest_Bike,
-        Drum_06,
-        PVE2_food_ration_02,
-        A04S01_f,
-        Wounded_05,
-        Drum_02,
-        Caves_18,
-        Bike_04,
-        PT_A01S12_RefugeeIsland,
-        Caves_2,
-        LegendaryQuest_AlbinoSpawner_Outside,
-        Kid_05,
-        ChallangeSP_FireCamp,
-        food_ration_16_mound,
-        BadWater_04,
-        LegendaryQuest_BadWaterBoatSpawner,
-        PVE2_food_ration_hanging_03,
-        Patrol_10_Totem_02,
-        A04S01_e,
-        Patrol_02_Totem_02,
-        PVE2_SmallAICamp_15,
-        PVE2_Wounded_05,
-        A03S01_OutofCenot,
-        Patrol_06_Totem_01,
-        BadWater_02,
-        PVE2_food_ration_04,
-        PVE2_food_ration_hanging_04,
-        Patrol_13_Totem_01,
-        Drum_04,
-        A04_S02_d_lake_canyons,
-        Caves_16,
-        Patrol_01_Totem_01,
-        food_ration_12_liane,
-        A01S08_CrashedPlane,
-        PVE2_Kid_03,
-        Albino_05,
-        PVE2_food_ration_hanging_01,
-        PVE2_Wounded_01_cave,
-        PVE2_SmallAICamp_19,
-        Wounded_03,
-        A04S01_f_Barrel2,
-        A03S02_TutorialCamp,
-        PoisonedWater02,
-        A04S02_d,
-        SmallAICamp_04,
-        Patrol_14_Totem_01,
-        SmallAICamp_07
+        A01S01_Harbor
+    , A01S02
+    , A01S03_Jeep
+    , A01S04
+    , A01S05_Puddle
+    , A01S06_Cartel
+    , A01S06_Cartel_Cave
+    , A01S07_GrabbingHook
+    , A01S07_RockToCartel
+    , A01S07_StoneRings
+    , A01S07_Village
+    , A01S08_CrashedPlane
+    , A01S09_Elevator
+    , A01S09_GoldMine
+    , A01S10_BambooBridge
+    , A01S10_EvilTribeCamp
+    , A01S11
+    , A01S11_EvilTribeCamp
+    , A01S11_SQPlace
+    , A01S12_GHtoAirport
+    , A01S12_Island
+    , A01S12_WhaCaveCamp
+    , A02S01_Airport
+    , A02S01_Cenot
+    , A02S02_Pond
+    , A03S01_Camp
+    , A03S01_CaveAyuhaska
+    , A03S01_GrabbHookHigh
+    , A03S01_OutofCenot
+    , A03S01_StoneRing
+    , A03S02_TutorialCamp
+    , A03S03_TribeVillage
+    , A04_S01_b_big_caves
+    , A04_S01_b_big_enemy_camp
+    , A04_S01_b_passage_to_A01_S07
+    , A04_S01_b_passage_to_A01_S12
+    , A04_S01_c_mangrove_border
+    , A04_S01_c_passage_to_A03_S03
+    , A04_S01_c_waterfall_caves
+    , A04_S01_c_waterfalls_island
+    , A04_S01_d_pve_boat
+    , A04_S01_d_steamboat
+    , A04_S01_e_giant_cave
+    , A04_S01_e_muddy_gorges
+    , A04_S01_f_hub_village
+    , A04_S01_f_passage_to_A01_S04
+    , A04_S01_f_shaman_passage
+    , A04_S01_g_passage_to_A01_S06
+    , A04_S01_g_POI_on_hills
+    , A04_S02_b_sacred_ruins
+    , A04_S02_b_stone_bridge
+    , A04_S02_c_dead_bodies_water_cave
+    , A04_S02_c_hot_river
+    , A04_S02_d_lake_canyons
+    , A04_S02_d_river_canyons_cave
+    , A04_S02_e_big_swamps
+    , A04_S02_e_river_canyons
+    , A04S01_a
+    , A04S01_b
+    , A04S01_c
+    , A04S01_c_Barrel1
+    , A04S01_c_Destroy
+    , A04S01_d
+    , A04S01_e
+    , A04S01_e_Barrel3
+    , A04S01_f
+    , A04S01_f_Barrel2
+    , A04S01_g
+    , A04S02_b
+    , A04S02_c
+    , A04S02_d
+    , A04S02_e
+    , Abandoned_Tribal_Village
+    , Airport
+    , Albino_01
+    , Albino_02
+    , Albino_03
+    , Albino_04
+    , Albino_05
+    , Albino_06
+    , Anaconda_Island
+    , BadWater_01
+    , BadWater_02
+    , BadWater_03
+    , BadWater_04
+    , BadWater_05
+    , BadWater_06
+    , BadWater_07
+    , Bamboo_Bridge
+    , Bamboo_Camp
+    , BigAICamp_02
+    , BigAICamp_03
+    , Bike_01
+    , Bike_02
+    , Bike_03
+    , Bike_04
+    , Bike_05
+    , Bike_06
+    , Bike_07
+    , Canoe_01
+    , Canoe_02
+    , Canoe_03
+    , Canoe_04
+    , Canoe_05
+    , Canoe_06
+    , Cartel_Antena
+    , Caves_1
+    , Caves_10
+    , Caves_11
+    , Caves_12
+    , Caves_13
+    , Caves_14
+    , Caves_15
+    , Caves_16
+    , Caves_17
+    , Caves_18
+    , Caves_2
+    , Caves_3
+    , Caves_4
+    , Caves_5
+    , Caves_6
+    , Caves_7
+    , Caves_8
+    , Caves_9
+    , ChallangeSP_Combat_Battery
+    , ChallangeSP_Combat_WT
+    , ChallangeSP_FireCamp
+    , ChallangeSP_MightyCamp
+    , ChallangeSP_Raft
+    , Custom
+    , Debug
+    , Drug_Facility
+    , Drum_01
+    , Drum_02
+    , Drum_03
+    , Drum_04
+    , Drum_05
+    , Drum_06
+    , Drum_07
+    , Drum_08
+    , East_Native_Camp
+    , Elevator_Cave
+    , food_ration_01_mound
+    , food_ration_02_mound
+    , food_ration_03_mound
+    , food_ration_04_mound
+    , food_ration_05_mound
+    , food_ration_06_liane
+    , food_ration_07_mound
+    , food_ration_08_mound
+    , food_ration_09_liane
+    , food_ration_10_mound
+    , food_ration_11_liane
+    , food_ration_12_liane
+    , food_ration_13_mound
+    , food_ration_14_mound
+    , food_ration_15_liane
+    , food_ration_16_mound
+    , food_ration_17_liane
+    , food_ration_18_mound
+    , food_ration_19_liane
+    , food_ration_20_liane
+    , food_ration_21_mound
+    , food_ration_22_liane
+    , food_ration_23_mound
+    , food_ration_24_liane
+    , food_ration_25_mound
+    , HangedBody_01
+    , HangedBody_02
+    , HangedBody_03
+    , HangedBody_04
+    , HangedBody_05
+    , HangedBody_06
+    , HangedBody_07
+    , HangedBody_08
+    , HangedBody_09
+    , HangedBody_10
+    , HangedBody_11
+    , HangedBody_12
+    , HangedBody_13
+    , Harbor
+    , Hub_Village_Outside
+    , Kid_01
+    , Kid_02
+    , Kid_03
+    , Kid_04
+    , Kid_05
+    , LegendaryQuest_AlbinoSpawner
+    , LegendaryQuest_AlbinoSpawner_Outside
+    , LegendaryQuest_BabyTapirSpawner1
+    , LegendaryQuest_BabyTapirSpawner2
+    , LegendaryQuest_BabyTapirSpawner3
+    , LegendaryQuest_BabyTapirSpawner4
+    , LegendaryQuest_BabyTapirSpawner5
+    , LegendaryQuest_BabyTapirSpawner6
+    , LegendaryQuest_BabyTapirSpawner7
+    , LegendaryQuest_BabyTapirSpawner8
+    , LegendaryQuest_BadWaterAltarSpawner
+    , LegendaryQuest_BadWaterBoatSpawner
+    , LegendaryQuest_BadWaterCaveSpawner
+    , LegendaryQuest_Bike
+    , LegendaryQuest_CanoeBoatSpawner
+    , LegendaryQuest_PantherSpawner
+    , LegendaryQuest_PantherSpawnerOutside
+    , Main_Tribal_Village
+    , MantisSpawner
+    , Native_Passage
+    , Omega_Camp
+    , Overturned_Jeep
+    , Panther_01
+    , Panther_02
+    , Panther_03
+    , Panther_04
+    , Panther_05
+    , Panther_06
+    , Panther_07
+    , Patrol_01_Totem_01
+    , Patrol_02_Totem_01
+    , Patrol_02_Totem_02
+    , Patrol_03_Totem_01
+    , Patrol_04_Totem_01
+    , Patrol_04_Totem_02
+    , Patrol_05_Totem_01
+    , Patrol_05_Totem_02
+    , Patrol_06_Totem_01
+    , Patrol_06_Totem_02
+    , Patrol_07_Totem_01
+    , Patrol_08_Totem_01
+    , Patrol_08_Totem_02
+    , Patrol_09_Totem_01
+    , Patrol_09_Totem_02
+    , Patrol_10_Totem_01
+    , Patrol_10_Totem_02
+    , Patrol_10_Totem_03
+    , Patrol_11_Totem_01
+    , Patrol_12_Totem_01
+    , Patrol_12_Totem_02
+    , Patrol_13_Totem_01
+    , Patrol_13_Totem_02
+    , Patrol_14_Totem_01
+    , Patrol_14_Totem_02
+    , Planecrash_Cave
+    , PoisonedWater01
+    , PoisonedWater02
+    , PoisonedWater03
+    , PoisonedWater04
+    , PoisonedWater05
+    , PoisonedWater06
+    , PoisonedWater07
+    , PoisonedWater08
+    , PoisonedWater09
+    , PoisonedWater10
+    , Pond
+    , PT_A01S01_CaveCamp
+    , PT_A01S01_Harbor
+    , PT_A01S01_Jungle
+    , PT_A01S01_TribeCamp
+    , PT_A01S02_Jungle
+    , PT_A01S03_Jeep
+    , PT_A01S03_Jungle
+    , PT_A01S05_Pond
+    , PT_A01S05_River
+    , PT_A01S06_Cartel
+    , PT_A01S06_Jungle
+    , PT_A01S07_Jungle
+    , PT_A01S07_Jungle2
+    , PT_A01S07_TribeVillage
+    , PT_A01S08_Jungle
+    , PT_A01S08_PlaneCrash
+    , PT_A01S09_Elevator
+    , PT_A01S09_GoldMine
+    , PT_A01S10_TribeCamp
+    , PT_A01S11_Jungle
+    , PT_A01S11_JungleBamboo
+    , PT_A01S12_RefugeeIsland
+    , PT_A01S12_WHACamp
+    , PT_A02S01_Airport
+    , PT_A02S01_Cenote
+    , PT_A02S01_Jungle
+    , PT_A02S02_Cenote
+    , PT_A02S02_Lake
+    , PT_A03S01_Rozlewiska
+    , PT_A03S01_WHACamp
+    , PT_A03S02_Tutorial
+    , PT_A03S03_Jungle
+    , PT_A03S03_TribeVillage
+    , Puddle
+    , PVE_Boat
+    , PVE_Map_Crate
+    , PVE_StartDebugSpawner
+    , PVE2_BigAICamp_03
+    , PVE2_BigAICamp_04
+    , PVE2_BigAICamp_05
+    , PVE2_food_ration_01
+    , PVE2_food_ration_02
+    , PVE2_food_ration_03
+    , PVE2_food_ration_04
+    , PVE2_food_ration_05
+    , PVE2_food_ration_06
+    , PVE2_food_ration_07
+    , PVE2_food_ration_08
+    , PVE2_food_ration_09
+    , PVE2_food_ration_10
+    , PVE2_food_ration_11
+    , PVE2_food_ration_12
+    , PVE2_food_ration_13
+    , PVE2_food_ration_14
+    , PVE2_food_ration_axe
+    , PVE2_food_ration_hanging_01
+    , PVE2_food_ration_hanging_02
+    , PVE2_food_ration_hanging_03
+    , PVE2_food_ration_hanging_04
+    , PVE2_food_ration_hanging_05
+    , PVE2_food_ration_hanging_06
+    , PVE2_food_ration_hanging_07
+    , PVE2_food_ration_hanging_08
+    , PVE2_food_ration_hanging_09
+    , PVE2_food_ration_hanging_10
+    , PVE2_Kid_01
+    , PVE2_Kid_02
+    , PVE2_Kid_03
+    , PVE2_Kid_04
+    , PVE2_SmallAICamp_11
+    , PVE2_SmallAICamp_12
+    , PVE2_SmallAICamp_13
+    , PVE2_SmallAICamp_14
+    , PVE2_SmallAICamp_15
+    , PVE2_SmallAICamp_16
+    , PVE2_SmallAICamp_17
+    , PVE2_SmallAICamp_18
+    , PVE2_SmallAICamp_19
+    , PVE2_SmallAICamp_20
+    , PVE2_Wounded_01
+    , PVE2_Wounded_01_cave
+    , PVE2_Wounded_02
+    , PVE2_Wounded_02_cave
+    , PVE2_Wounded_03
+    , PVE2_Wounded_03_cave
+    , PVE2_Wounded_04
+    , PVE2_Wounded_04_cave
+    , PVE2_Wounded_05
+    , PVE2_Wounded_05_cave
+    , QA_Test
+    , Scorpion_Cartel_Cave
+    , SmallAICamp_01
+    , SmallAICamp_01_outer
+    , SmallAICamp_02
+    , SmallAICamp_03
+    , SmallAICamp_04
+    , SmallAICamp_05
+    , SmallAICamp_06
+    , SmallAICamp_07
+    , SmallAICamp_08
+    , SmallAICamp_09
+    , SmallAICamp_10
+    , Spikes_Tree_Review_Spawner
+    , Statue_01
+    , Statue_02
+    , Statue_03
+    , Statue_04
+    , Statue_05
+    , Statue_06
+    , Statue_07
+    , Story_Start_Oasis
+    , Teleport_Start_Location
+    , Tutorial_Camp
+    , Village_02_inside
+    , Village_02_outside
+    , West_Native_Camp
+    , WomanInCage_01
+    , WomanInCage_02
+    , WomanInCage_03
+    , Wounded_01
+    , Wounded_02
+    , Wounded_03
+    , Wounded_04
+    , Wounded_05
     }
 
     public enum MessageType
